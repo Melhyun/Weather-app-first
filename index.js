44f3824aac93f18ab1a2308f1801220fbaad3f81
Melhyun <melika.bastani@gmail.com>
2024-09-10 03:04:37 +0330

    removed console.log

--- index.js
+++ index.js
@@ -74,8 +74,6 @@ function getForecast(city) {
 }
 
 function displayForecast(response) {
-  console.log(response.data);
-
   let forecastHtml = "";
 
   response.data.daily.forEach(function (day, index) {
